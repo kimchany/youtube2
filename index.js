51af4752a0b8c8962ae9e3f03e31d284c867782a
username <55888573+kimchany@users.noreply.github.com>
2020-01-18 02:03:44 +0900

    first
    2020-01-18

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hi!");
+import express from "express";
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () => 
+    console.log(`Listening on: http://localhost:${PORT}`);
+
+const handleHome = (req, res) => res.send("Hi from home!!");
+
+const handleProfile = (req, res) => res.send("This is profile.");
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
